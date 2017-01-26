f648af5688baef45521a5226e93d4baf47ef9fc7
37pin <ucorpor@gmail.com>
2017-01-26 16:45:37 +0400

    Fix format

--- index.js
+++ index.js
@@ -21,44 +21,44 @@ request('https://api.tjournal.ru/2.3/club?count=30&sortMode=recent', function (e
                     "subtitle": "Французский видеохостинг Dailymotion сообщил о планах избежать блокировки в России"
                   },
                   "id": 40276,
-		  "fullWidth": false
+                  "fullWidth": false
                 }
-              }
-            ],
-            "children": [
-              {
-                "elementName": "CardHeader",
-                "attributes": {
-                  "title": "Французский видеохостинг Dailymotion сообщил о планах избежать блокировки в России",
-                  "subtitle": "Wed, 25 Jan 2017"
-                }
-              },
-              {
-                "elementName": "CardBodyText",
-                "attributes": {
-                  "text": "Представители Dailymotion заявили, что компания не знает о блокировке в России и планирует её избежать. Ранее Мосгорсуд постановил «навечно» заблокировать сервис в России за нелегальное распространение нескольких шоу телеканала «Пятница!». Об этом сообщают «Ведомости»."
-                }
-              },
-              {
-                "elementName": "CardImage",
-                "attributes": {
-                  "width": 12,
-                  "uri": "https://static30.cmtt.ru/club/1b/26/35/580d7b5ef08d4e.png",
-                  //"hidpiUri": "…",
-                  "height": 12
-                }
-              },
-              {
-                "elementName": "CardFooter",
-                "attributes": {
-                  "labels": [
-                    {
-                      "name": "13",
-                      "color": "#42a5f5"
+                "children": [
+                  {
+                    "elementName": "CardHeader",
+                    "attributes": {
+                      "title": "Французский видеохостинг Dailymotion сообщил о планах избежать блокировки в России",
+                      "subtitle": "Wed, 25 Jan 2017"
                     }
-                  ],
-                  "footnote": "Roman Persianinov"
-                }
+                  },
+                  {
+                    "elementName": "CardBodyText",
+                    "attributes": {
+                      "text": "Представители Dailymotion заявили, что компания не знает о блокировке в России и планирует её избежать. Ранее Мосгорсуд постановил «навечно» заблокировать сервис в России за нелегальное распространение нескольких шоу телеканала «Пятница!». Об этом сообщают «Ведомости»."
+                    }
+                  },
+                  {
+                    "elementName": "CardImage",
+                    "attributes": {
+                      "width": 12,
+                      "uri": "https://static30.cmtt.ru/club/1b/26/35/580d7b5ef08d4e.png",
+                      //"hidpiUri": "…",
+                      "height": 12
+                    }
+                  },
+                  {
+                    "elementName": "CardFooter",
+                    "attributes": {
+                      "labels": [
+                        {
+                          "name": "13",
+                          "color": "#42a5f5"
+                        }
+                      ],
+                      "footnote": "Roman Persianinov"
+                    }
+                  }
+                ]
               }
             ]
           }
