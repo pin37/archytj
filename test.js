c4ee9ce0a7dd6b891dd94081c11ae2e6bce2f2ba
37pin <ucorpor@gmail.com>
2017-01-27 13:57:22 +0400

    True JSON response

--- test.js
+++ test.js
@@ -1,19 +1,2 @@
-var timeStart = new Date("Mon Jan 01 2007 11:00:00 GMT+0530").getTime();
-var timeEnd = new Date("Mon Jan 01 2007 11:30:00 GMT+0530").getTime();
-var hourDiff = timeEnd - timeStart; //in ms
-var secDiff = hourDiff / 1000; //in s
-var minDiff = hourDiff / 60 / 1000; //in minutes
-var hDiff = hourDiff / 3600 / 1000; //in hours
-var humanReadable = {};
-humanReadable.hours = Math.floor(hDiff);
-humanReadable.minutes = minDiff - 60 * humanReadable.hours;
-console.log(humanReadable); //{hours: 0, minutes: 30}
-
-/*var today = new Date();
-var Christmas = new Date("12-25-2012");
-var diffMs = (Christmas - today); // milliseconds between now & Christmas
-var diffDays = Math.floor(diffMs / 86400000); // days
-var diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
-var diffMins = Math.floor(((diffMs % 86400000) % 3600000) / 60000); // minutes
-console.log(diffDays + " days, " + diffHrs + " hours, " + diffMins + " minutes until Christmas 2009 =)");
-*/
+var str = 'button onClick="dostuff(\'>\');"></button>';
+console.log(str.replace(/<(?:.|\n)*?>/gm, ''));
