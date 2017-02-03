45b4a5bac7713892fd3b77829e1dcc3a358bd24a
37pin <ucorpor@gmail.com>
2017-02-03 16:11:39 +0400

    Added categories filter

--- main.js
+++ main.js
@@ -1,152 +1,37 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
   fetch = require('node-fetch'),
-  striptags = require('striptags'),
   buildUrl = require('build-url'),
-  app = express();
+  utils = require('./utils'),
+  app = express()
+let requestsCount = 0;
 app.use(bodyParser.json());
 
-const limit = 25;
-let requestsCount = 0;
-app.post('/archytj/', function (request, response) {
-  if (!request.body.payload) {
-    request.body.payload = { user: { id: null} };
-  }
-  console.log('Request #' + ++requestsCount + ' from user with id: ' + request.body.payload.user.id);
-  const page = request.body.payload.nextResultCursor || { after: 0, limit: limit };
-  const url = buildUrl('https://api.tjournal.ru', {
-    path: '2.3/club',
-    queryParams: {
-        sortMode: 'recent',
-        count: page.limit,
-        offset: page.after
-      }
-  });
-  fetch(url).then(res => res.json())
-    .then(json => makeResponse(json, response, page));
-});
+app.post('/archytj/', (request, response) => sendRequest(request, response, 0));
+app.post('/archytj/news', (request, response) => sendRequest(request, response, 1));
+app.post('/archytj/offtop', (request, response) => sendRequest(request, response, 2));
+app.post('/archytj/videos', (request, response) => sendRequest(request, response, 3));
+app.post('/archytj/articles', (request, response) => sendRequest(request, response, 4));
 
 app.listen(3000, function () {
   console.log('Server is running...');
 });
 
-function makeResponse(json, response, page) {
-  page.after += limit;
-  const cards = createElement('Cards', null, []);
-  const result = createElement('Result', { meta: { title: 'TJ' }, nextResultCursor: page }, [cards]);
-
-  json.forEach(function(article, i, json) {
-    result.children[0].children.push(toCard(article));
-  });
-  response.json(result);
-}
-
-function createElement(elementName, attributes, children) { 
-  return {
-    elementName: elementName,
-    attributes: attributes,
-    children: children
-  };
-};
-
-function toCard(article) {
-  // header
-  const title = article.title,
-    type = article.type;
-  let category = 'News';
-  if (type === 4) {
-    category = 'Article';
-  } else if (type === 3) {
-    category = 'Video';
-  } else if (type === 2) {
-    category = 'Off topic';
+function sendRequest(request, response, type) {
+  if (!request.body.payload) {
+    request.body.payload = { user: { id: null} };
   }
-  const header = createElement('CardHeader', { title: title, subtitle: category });
-
-  // author
-  const userDevice = article.userDevice;
-  let device = 'desktop';
-  if (userDevice === 2) {
-    device = 'apple';
-  } else if (userDevice === 1) {
-    device = 'android';
-  }
-  const timestamp = new Date(article.date * 1000);
-  const authorAttributes = {
-    iconName: device,
-    color: "#bdbdbd",
-    created: {
-      by: article.publicAuthor.name,
-      at: timestamp
-    }
-  };
-  const author = createElement('Media', authorAttributes);
-
-  // text
-  const text = createElement('CardBodyText', { text: striptags(article.intro) });
-
-  // image
-  let image = null;
-  const cover = article.cover;
-  if (cover && cover.size) {
-    const imageAttributes = {
-      width: cover.size.width,
-      height: cover.size.height,
-      uri: cover.thumbnailUrl
-    }
-    image = createElement('CardImage', imageAttributes);
-  }
-
-  // footer
-  const likesCount = article.likes.summ;
-  let like = '—';
-  if (likesCount > 0) {
-    like = '👍+' + likesCount;
-  } else if (likesCount < 0) {
-    like = '👎' + likesCount;
-  }
-  const footerAttributes = {
-    labels: [
-      {
-        name: like,
-        color: 'ffffff'
-      },
-      {
-        name: '👁' + article.hits,
-        color: 'ffffff'
-      },
-      {
-        name: '💬' + article.commentsCount,
-        color: 'ffffff'
+  console.log('Request #' + ++requestsCount + ' from user with id: ' + request.body.payload.user.id);
+  const page = request.body.payload.nextResultCursor || { after: 0, limit: utils.limit };
+  const url = buildUrl('https://api.tjournal.ru', {
+    path: '2.3/club',
+    queryParams: {
+        sortMode: 'recent',
+        count: page.limit,
+        offset: page.after,
+        type: type
       }
-    ]
-  };
-  const isPopular = article.isReadMore;
-  if (isPopular) {
-    footerAttributes.labels.push(
-      {
-        name: '🔥hot',
-        color: 'ffffff'
-      });
-  }
-  if (article.isAdvertising) {
-    footerAttributes.labels.push(
-      {
-        name: '💵ad',
-        color: '#ffffff'
-      });
-  }
-  const footer = createElement('CardFooter', footerAttributes);
-
-  // card
-  const cardAttributes = {
-    id: article.id,
-    uri: article.url,
-    timestamp: timestamp
-  };
-  if (isPopular) {
-    cardAttributes.pushNotification = { title: 'TJ', subtitle: title };
-  }
-  const card = createElement('Card', cardAttributes, [header, author, text, image, footer]);
-  return card;
+  });
+  fetch(url).then(res => res.json())
+    .then(json => utils.makeResponse(json, response, page, type));
 }
---
+++ utils.js
@@ -0,0 +1,179 @@
+const striptags = require('striptags'),
+  limit = 25,
+  filters = [
+    {
+      address: '/',
+      meta: {
+        title: 'All',
+        subscribe: true
+      }
+    }, {
+      address: '/articles/',
+      meta: {
+        title: 'Articles'
+      }
+    }, {
+      address: '/videos/',
+      meta: {
+        title: 'Videos'
+      }
+    }, {
+      address: '/offtop/',
+      meta: {
+        title: 'Off topic'
+      }
+    }, {
+      address: '/news/',
+      meta: {
+        title: 'News'
+      }
+    }
+  ];
+
+function createElement(elementName, attributes, children) { 
+  return {
+    elementName: elementName,
+    attributes: attributes,
+    children: children
+  };
+}
+
+function removeFilter(category) {
+  if (category === 'TJ') category = 'all';
+  const array = filters.slice();
+  array.filter((value, i) => {
+    if (value.meta.title.toLowerCase() === category.toLowerCase()) {
+      array.splice(i, 1);
+    }
+  });
+  return array;
+}
+
+function makeResponse(json, response, page, type) {
+  page.after += limit;
+  const cards = createElement('Cards', null, []);
+  let filterCategory = 'TJ';
+  if (type === 4) {
+    filterCategory = 'Articles';
+  } else if (type === 3) {
+    filterCategory = 'Videos';
+  } else if (type === 2) {
+    filterCategory = 'Off topic';
+  } else if (type === 1) {
+    filterCategory = 'News';
+  }
+  const resultAttributes = {
+    meta: { title: filterCategory },
+    links: removeFilter(filterCategory),
+    nextResultCursor: page
+  };
+  const result = createElement('Result', resultAttributes, [cards]);
+
+  json.forEach(function(article, i, json) {
+    result.children[0].children.push(toCard(article));
+  });
+  response.json(result);
+}
+
+function toCard(article) {
+  // header
+  const title = article.title,
+    type = article.type;
+  let category = 'News';
+  if (type === 4) {
+    category = 'Article';
+  } else if (type === 3) {
+    category = 'Video';
+  } else if (type === 2) {
+    category = 'Off topic';
+  }
+  const header = createElement('CardHeader', { title: title, subtitle: category });
+
+  // author
+  const userDevice = article.userDevice;
+  let device = 'desktop';
+  if (userDevice === 2) {
+    device = 'apple';
+  } else if (userDevice === 1) {
+    device = 'android';
+  }
+  const timestamp = new Date(article.date * 1000);
+  const authorAttributes = {
+    iconName: device,
+    color: "#bdbdbd",
+    created: {
+      by: article.publicAuthor.name,
+      at: timestamp
+    }
+  };
+  const author = createElement('Media', authorAttributes);
+
+  // text
+  const text = createElement('CardBodyText', { text: striptags(article.intro) });
+
+  // image
+  let image = null;
+  const cover = article.cover;
+  if (cover && cover.size) {
+    const imageAttributes = {
+      width: cover.size.width,
+      height: cover.size.height,
+      uri: cover.thumbnailUrl
+    }
+    image = createElement('CardImage', imageAttributes);
+  }
+
+  // footer
+  const likesCount = article.likes.summ;
+  let like = '—';
+  if (likesCount > 0) {
+    like = '👍+' + likesCount;
+  } else if (likesCount < 0) {
+    like = '👎' + likesCount;
+  }
+  const footerAttributes = {
+    labels: [
+      {
+        name: like,
+        color: 'ffffff'
+      }, {
+        name: '👁' + article.hits,
+        color: 'ffffff'
+      }, {
+        name: '💬' + article.commentsCount,
+        color: 'ffffff'
+      }
+    ]
+  };
+  const isPopular = article.isReadMore;
+  if (isPopular) {
+    footerAttributes.labels.push(
+      {
+        name: '🔥hot',
+        color: 'ffffff'
+      });
+  }
+  if (article.isAdvertising) {
+    footerAttributes.labels.push(
+      {
+        name: '💵ad',
+        color: '#ffffff'
+      });
+  }
+  const footer = createElement('CardFooter', footerAttributes);
+
+  // card
+  const cardAttributes = {
+    id: article.id,
+    uri: article.url,
+    timestamp: timestamp
+  };
+  if (isPopular) {
+    cardAttributes.pushNotification = { title: 'TJ', subtitle: title };
+  }
+  const card = createElement('Card', cardAttributes, [header, author, text, image, footer]);
+  return card;
+}
+
+exports.limit = limit;
+exports.makeResponse = makeResponse;
